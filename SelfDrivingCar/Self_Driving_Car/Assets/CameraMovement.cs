977642c181f423fb4096c4d685ab9da00d5eaf9e
Bazsa47 <farkasbalazs0127@gmail.com>
2021-10-14 21:27:49 +0200

    init

---
+++ SelfDrivingCar/Self_Driving_Car/Assets/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject redCar;
+    public GameObject greenCar;
+    public Camera greenCamera;
+    float redCarFitness, greenCarFitness;
+
+     void Start()
+    {
+
+        greenCamera.enabled = true;
+    }
+    void FixedUpdate()
+    {
+        redCarFitness = redCar.GetComponent<RedCarController>().overallFitness;
+        greenCarFitness = greenCar.GetComponent<CarController>().overallFitness;
+
+        if(redCarFitness > greenCarFitness)
+        {
+            greenCamera.enabled = false;
+        }
+        else
+        {
+            greenCamera.enabled = true;
+        }
+    }
+}
---
+++ SelfDrivingCar/Self_Driving_Car/Assets/UIScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIScript : MonoBehaviour
+{
+    public GeneticManager greenManager;
+    public RedGeneticManager redManager;
+    public TextMeshProUGUI redText, greenText, timeScale;
+    private int redGeneration, redGenome, greenGeneration, greenGenome;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        redGeneration = redManager.currentGeneration;
+        redGenome = redManager.currentGenome;
+        greenGeneration = greenManager.currentGeneration;
+        greenGenome = greenManager.currentGenome;
+        redText.text = string.Format("Generation: {0} Genome: {1}",redGeneration,redGenome);
+        greenText.text = string.Format("Generation: {0} Genome: {1}", greenGeneration, greenGenome);
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && Time.timeScale > 1)
+        {
+            Time.timeScale -= 0.25f;
+            timeScale.text = "Time scale: " + Time.timeScale+"x";
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Time.timeScale <= 20){
+            Time.timeScale += 0.25f;
+            timeScale.text = "Time scale: " + Time.timeScale+ "x";
+        }
+    }
+}
